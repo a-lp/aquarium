35979f6323a6aee9f975e0063abe237a2ab4d605
Armando <armando.laplaca@gmail.com>
2020-02-05 23:51:40 +0100

    add sorting on fishes table
    clicking the headers will now sort in ascendent/descendent order for that column

--- src/main/angular/src/app/fishes/fishes.component.ts
+++ src/main/angular/src/app/fishes/fishes.component.ts
@@ -10,6 +10,9 @@ import {FishService} from '../service/fish.service';
 export class FishesComponent implements OnInit {
   fishes: Array<Fish>;
 
+  field: number = 0; // Sorting field
+  ascendent: boolean = true;
+
   // retiredFishes: Array<Fish>;
 
   constructor(private animalService: FishService) {
@@ -37,4 +40,46 @@ export class FishesComponent implements OnInit {
 
   }
 
+
+  sort(field: number) {
+    console.log(field);
+    if (this.field == field) {
+      this.ascendent = !this.ascendent;
+    } else {
+      this.ascendent = true;
+    }
+    this.field = field;
+    switch (field) {
+      case 0:
+        this.fishes = this.fishes.sort((a, b) => (this.ascendent ? 1 : -1) * a.id.toString().localeCompare(b.id.toString()));
+        break;
+      case 1:
+        this.fishes = this.fishes.sort((a, b) => (this.ascendent ? 1 : -1) * a.name.localeCompare(b.name));
+        break;
+      case 2:
+        this.fishes = this.fishes.sort((a, b) => (this.ascendent ? 1 : -1) * a.gender.localeCompare(b.gender));
+        break;
+      case 3:
+        this.fishes = this.fishes.sort((a, b) => (this.ascendent ? 1 : -1) * (a.arrivalDate == b.arrivalDate ? 0 : (a.arrivalDate > b.arrivalDate ? 1 : -1)));
+        break;
+      case 4:
+        this.fishes = this.fishes.sort((a, b) => (this.ascendent ? 1 : -1) * (a.returnDate == b.returnDate ? 0 : (a.returnDate > b.returnDate ? 1 : -1)));
+        break;
+      case 5:
+        this.fishes = this.fishes.sort((a, b) => (this.ascendent ? 1 : -1) * a.specie.name.localeCompare(b.specie.name));
+        break;
+      case 6:
+        this.fishes = this.fishes.sort((a, b) => {
+          console.log(a, b)
+          if (a.pool == null) {
+            return (this.ascendent ? 1 : -1);
+          }
+          if (b.pool == null) {
+            return (this.ascendent ? -1 : 1);
+          }
+          return (this.ascendent ? 1 : -1) * a.pool.id.toString().localeCompare(b.pool.id.toString());
+        });
+        break;
+    }
+  }
 }
