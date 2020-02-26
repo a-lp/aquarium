f952c7c9e191130f9bee6f85367664df5452d3c6
Armando <armando.laplaca@gmail.com>
2020-02-26 23:35:19 +0100

    Fix of null responses after authentication requests

--- src/main/java/fr/upem/devops/security/TokenAuthenticationFilter.java
+++ src/main/java/fr/upem/devops/security/TokenAuthenticationFilter.java
@@ -6,8 +6,11 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
 import org.springframework.security.web.util.matcher.RequestMatcher;
 
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.util.Optional;
 
 public class TokenAuthenticationFilter extends AbstractAuthenticationProcessingFilter {
@@ -30,4 +33,13 @@ public class TokenAuthenticationFilter extends AbstractAuthenticationProcessingF
         return getAuthenticationManager().authenticate(auth);
     }
 
+    @Override
+    protected void successfulAuthentication(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain chain,
+            Authentication authResult) throws IOException, ServletException {
+        super.successfulAuthentication(request, response, chain, authResult);
+        chain.doFilter(request, response);
+    }
 }
