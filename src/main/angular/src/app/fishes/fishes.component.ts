23690fce383d33c7d8140a44bff7244afe05ed28
Armando <armando.laplaca@gmail.com>
2020-03-01 21:35:19 +0100

    Add fishes visitor page.

--- src/main/angular/src/app/fishes/fishes.component.ts
+++ src/main/angular/src/app/fishes/fishes.component.ts
@@ -1,4 +1,7 @@
 import {Component, OnInit} from '@angular/core';
+import {Fish} from "../model/Fish";
+import {FishService} from "../service/fish.service";
+import {DatePipe} from "@angular/common";
 
 @Component({
   selector: 'app-fishes',
@@ -6,12 +9,23 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./fishes.component.css']
 })
 export class FishesComponent implements OnInit {
+  fishes: Array<Fish> = [];
 
-  constructor() {
+  constructor(private fishService: FishService, private datePipe: DatePipe) {
   }
 
   ngOnInit() {
-
+    this.refresh();
   }
 
+  refresh() {
+    this.fishService.getAll().subscribe(
+      data => {
+        console.log(data);
+        if (data != null) {
+          this.fishes = data;
+        }
+      }
+    );
+  }
 }
